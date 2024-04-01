d45656143b6f735e663efda926daf3162cba6019
Chris Adongo <119563006+ChrisAdongo@users.noreply.github.com>
2024-04-01 14:05:10 +0300

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Unit Test for City Class
+Unit Test for Place Class
 """
 from datetime import datetime
 import inspect
@@ -10,78 +10,78 @@ from os import environ, stat
 import pep8
 import unittest
 
-City = models.city.City
+Place = models.place.Place
 BaseModel = models.base_model.BaseModel
 STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
 
 
-class TestCityDocs(unittest.TestCase):
+class TestPlaceDocs(unittest.TestCase):
     """Class for testing BaseModel docs"""
 
-    all_funcs = inspect.getmembers(City, inspect.isfunction)
+    all_funcs = inspect.getmembers(Place, inspect.isfunction)
 
     @classmethod
     def setUpClass(cls):
         print('\n\n.................................')
         print('..... Testing Documentation .....')
-        print('........   City Class   ........')
+        print('........   Place Class   ........')
         print('.................................\n\n')
 
     def test_doc_file(self):
         """... documentation for the file"""
-        expected = '\nCity Class from Models Module\n'
-        actual = models.city.__doc__
+        expected = '\nPlace Class from Models Module\n'
+        actual = models.place.__doc__
         self.assertEqual(expected, actual)
 
     def test_doc_class(self):
         """... documentation for the class"""
-        expected = 'City class handles all application cities'
-        actual = City.__doc__
+        expected = 'Place class handles all application places'
+        actual = Place.__doc__
         self.assertEqual(expected, actual)
 
     def test_all_function_docs(self):
         """... tests for ALL DOCS for all functions in db_storage file"""
-        all_functions = TestCityDocs.all_funcs
+        all_functions = TestPlaceDocs.all_funcs
         for function in all_functions:
             self.assertIsNotNone(function[1].__doc__)
 
-    def test_pep8_city(self):
-        """... city.py conforms to PEP8 Style"""
+    def test_pep8_place(self):
+        """... place.py conforms to PEP8 Style"""
         pep8style = pep8.StyleGuide(quiet=True)
-        errors = pep8style.check_files(['models/city.py'])
+        errors = pep8style.check_files(['models/place.py'])
         self.assertEqual(errors.total_errors, 0, errors.messages)
 
     def test_file_is_executable(self):
         """... tests if file has correct permissions so user can execute"""
-        file_stat = stat('models/city.py')
+        file_stat = stat('models/place.py')
         permissions = str(oct(file_stat[0]))
         actual = int(permissions[5:-2]) >= 5
         self.assertTrue(actual)
 
 
-class TestCityInstances(unittest.TestCase):
+class TestPlaceInstances(unittest.TestCase):
     """testing for class instances"""
 
     @classmethod
     def setUpClass(cls):
         print('\n\n.................................')
         print('....... Testing Functions .......')
-        print('.........  City Class  .........')
+        print('.........  Place Class  .........')
         print('.................................\n\n')
 
     def setUp(self):
-        """initializes new city for testing"""
-        self.city = City()
+        """initializes new place for testing"""
+        self.place = Place()
 
     def test_instantiation(self):
-        """... checks if City is properly instantiated"""
-        self.assertIsInstance(self.city, City)
+        """... checks if Place is properly instantiated"""
+        self.assertIsInstance(self.place, Place)
 
     @unittest.skipIf(STORAGE_TYPE == 'db', 'skip if environ is db')
     def test_to_string(self):
         """... checks if BaseModel is properly casted to string"""
-        my_str = str(self.city)
-        my_list = ['City', 'id', 'created_at']
+        my_str = str(self.place)
+        my_list = ['Place', 'id', 'created_at']
         actual = 0
         for sub_str in my_list:
             if sub_str in my_str:
@@ -91,8 +91,7 @@ class TestCityInstances(unittest.TestCase):
     @unittest.skipIf(STORAGE_TYPE == 'db', 'skip if environ is db')
     def test_instantiation_no_updated(self):
         """... should not have updated attribute"""
-        self.city = City()
-        my_str = str(self.city)
+        my_str = str(self.place)
         actual = 0
         if 'updated_at' in my_str:
             actual += 1
@@ -101,40 +100,40 @@ class TestCityInstances(unittest.TestCase):
     @unittest.skipIf(STORAGE_TYPE == 'db', 'skip if environ is db')
     def test_updated_at(self):
         """... save function should add updated_at attribute"""
-        self.city.save()
-        actual = type(self.city.updated_at)
+        self.place.save()
+        actual = type(self.place.updated_at)
         expected = type(datetime.now())
         self.assertEqual(expected, actual)
 
     @unittest.skipIf(STORAGE_TYPE == 'db', 'skip if environ is db')
     def test_to_json(self):
         """... to_json should return serializable dict object"""
-        self.city_json = self.city.to_json()
+        self.place_json = self.place.to_json()
         actual = 1
         try:
-            serialized = json.dumps(self.city_json)
+            serialized = json.dumps(self.place_json)
         except:
             actual = 0
         self.assertTrue(1 == actual)
 
     @unittest.skipIf(STORAGE_TYPE == 'db', 'skip if environ is db')
     def test_json_class(self):
-        """... to_json should include class key with value City"""
-        self.city_json = self.city.to_json()
+        """... to_json should include class key with value Place"""
+        self.place_json = self.place.to_json()
         actual = None
-        if self.city_json['__class__']:
-            actual = self.city_json['__class__']
-        expected = 'City'
+        if self.place_json['__class__']:
+            actual = self.place_json['__class__']
+        expected = 'Place'
         self.assertEqual(expected, actual)
 
-    def test_state_attribute(self):
-        """... add state attribute"""
-        self.city.state_id = 'IL'
-        if hasattr(self.city, 'state_id'):
-            actual = self.city.state_id
+    def test_guest_attribute(self):
+        """... add guest attribute"""
+        self.place.max_guest = 3
+        if hasattr(self.place, 'max_guest'):
+            actual = self.place.max_guest
         else:
             actual = ''
-        expected = 'IL'
+        expected = 3
         self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
